93488da93d48564ce7df6db13596fc4c0a0b9f19
Jose Barrera <jfbarrera2002@gmail.com>
2025-09-14 18:09:05 -0500

    feat: implemented api documentation and configured cors
    Configured drf-spectacular and django-cors-headers in our settings. API documentation can now be viewed and the schema may be downloaded. Request can also be made from allowed origins.
    
    Closes #7

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -26,6 +26,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include("post.urls")),
     path('markdownx/', include('markdownx.urls')),
+    
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ] + debug_toolbar_urls()
 
 # Serve media in development so uploaded images render
